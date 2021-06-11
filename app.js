fd32540aa80d5bbdf6c4e99b92aa85597c7a5894
Gallahad072 <gallahad072@gmail.com>
2021-06-11 14:13:23 +0100

    fully functional

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-const user_score = 0
-const machine_score = 0
+let user_score = 0
+let machine_score = 0
 const user_score_span = document.getElementById('user_score')
 const machine_score_span = document.getElementById('machine_score')
 const scoreboard_div = document.querySelector('.scoreboard')
 const result_div = document.querySelector('.result')
+result_div.innerHTML = "Make Your Move"
 const rock_div = document.getElementById('r')
 const paper_div = document.getElementById('p')
 const scissors_div = document.getElementById('s')
@@ -28,23 +29,57 @@ function machine_choice(){
     return array[random]
 }
 
+function combination(user, machine){
+    const game_set = new Set([user, machine])
+    if (game_set.has('r') && game_set.has('p')){
+        var action = 'Paper Covers Rock.'
+    }
+    else if (game_set.has('r') && game_set.has('s')){
+        var action = 'Rock Crushes Scissors.'
+    }
+    else{
+        var action = 'Scissors Cuts Paper.'
+    }
+    return action
+}
+
+function win(user, machine){
+    const end = ' You Win!'
+    const start = combination(user, machine)
+    user_score++
+    user_score_span.innerHTML = user_score
+    result_div.innerHTML = start+end
+}
+
+function lose(user, machine){
+    const end = ' You Lose!'
+    const start = combination(user, machine)
+    machine_score++
+    machine_score_span.innerHTML = machine_score
+    result_div.innerHTML = start+end
+}
+
+function tie(){
+    result_div.innerHTML = "It's a Tie!"
+}
+
 function game(user){
     const machine = machine_choice()
     switch(user+machine){
         case 'rs':
         case 'pr':
         case 'sp':
-            console.log('user wins')
+            win(user, machine)
             break
         case 'rp':
         case 'ps':
         case 'sr':
-            console.log('machine wins')
+            lose(user, machine)
             break
         case 'rr':
         case 'pp':
         case 'ss':
-            console.log('Tie')
+            tie()
             break
     }
 }
